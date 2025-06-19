d6926feea0779166c126b4a5c9d42e02474bc508
Manuel <jmrodriguez@aicox.com>
2025-06-19 12:27:03 +0200

    scss básico

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +6,29 @@ import { Component } from '@angular/core';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected title = 'style-test';
+
+export class AppComponent {
+  theme: 'theme-light' | 'theme-dark' = 'theme-light';
+
+  isDarkTheme = true;
+
+  constructor(private renderer: Renderer2) {}
+
+  toggleTheme() {
+    const body = document.querySelector('body'); // Seleccionamos el body
+  
+    if (body) {
+      // Reemplaza directamente theme-dark por theme-light o viceversa
+      if (body.classList.contains('theme-dark')) {
+        body.classList.replace('theme-dark', 'theme-light');
+        this.isDarkTheme = false;
+      } else {
+        body.classList.replace('theme-light', 'theme-dark');
+        this.isDarkTheme = true;
+      }
+    }
+  
+  }
 }
+
+
